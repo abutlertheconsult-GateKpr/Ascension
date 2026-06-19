c3c938295efe152474333d8bb943eef2295664a6
abutlertheconsult-GateKpr <abutler.theconsult@gmail.com>
2026-06-19 13:33:09 -0400

    revert report to operate as setup

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Caches all app files for full offline use after first load
 // ============================================================
 
-const CACHE_NAME = 'ascension-v4';
+const CACHE_NAME = 'ascension-v5.S00.L00.F00';
 
 // Files to cache on install
 const ASSETS = [
